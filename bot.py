036e34efe54c1a54eaf772cfe2211a1071f6e97d
erkin98 <erkinqara@gmail.com>
2021-06-03 15:05:03 +0400

    update bot.py

--- bot.py
+++ bot.py
@@ -15,24 +15,31 @@ def bot():
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
-    if 'salam' in incoming_msg:
-        # return a quote
-       
-        quote = 'Salam,bizə yazdığınız üçün təşəkkürlər.Sizə necə köməklik edə bikərik?'
+    if incoming_msg.isdigit():
+    
+        if int(incoming_msg) >= 15000:
+            quote = "Sizə göndərdiyimiz fayldan xidmətlərimizlə tanış ola bilərsiz"
+            msg.body(quote)
+            responded = True
+        elif int(incoming_msg) < 10000:
+            quote = "Sizin məbləğ üçün az sayda universitet var."
+            msg.body(quote)
+            responded = True
+    else:
+        if 'salam' in incoming_msg:
+            # return a quote
+        
+            quote = 'Salam,bizə yazdığınız üçün təşəkkürlər.Sizə necə köməklik edə bikərik?'
 
-        msg.body(quote)
-        responded = True
-    if 'xaricdə təhsil' or "xaricde tehsil" in incoming_msg:
-        # return a cat pic
-        # msg.media('https://cataas.com/cat')
-        quote = 'Xaricdə təhsil üçün büdcəniz nə qədərdir?'
+            msg.body(quote)
+            responded = True
+        elif 'xaricdə təhsil' or "xaricde tehsil" in incoming_msg:
+            # return a cat pic
+            # msg.media('https://cataas.com/cat')
+            quote = 'Xaricdə təhsil üçün büdcəniz nə qədərdir?'
 
-        msg.body(quote)
+            msg.body(quote)
 
-        responded = True
-    if incoming_msg > 15000:
-        quote = "Sizə göndərdiyimiz fayldan xidmətlərimizlə tanış ola bilərsiz"
+            responded = True
 
-    if incoming_msg < 15000:
-        quote = "Sizin məbləğ üçün az sayda universitet var."
-
+    return str(resp)
