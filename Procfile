dd159221d4b31060076aac890052b836c62134c0
unknown <erkinqara@gmail.com>
2021-06-03 12:02:44 +0400

    Initial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn -b :5000 bot:app --log-file=-
---
+++ bot.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello,bot'
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if 'salam' in incoming_msg:
+        # return a quote
+       
+        quote = 'Salam,bizə yazdığınız üçün təşəkkürlər.Sizə necə köməklik edə bikərik?'
+
+        msg.body(quote)
+        responded = True
+    if 'xaricdə təhsil' or "xaricde tehsil" in incoming_msg:
+        # return a cat pic
+        # msg.media('https://cataas.com/cat')
+        quote = 'Xaricdə təhsil üçün büdcəniz nə qədərdir?'
+
+        msg.body(quote)
+
+        responded = True
+    if incoming_msg > 15000:
+        quote = "Sizə göndərdiyimiz fayldan xidmətlərimizlə tanış ola bilərsiz"
+
+    if incoming_msg < 15000:
+        quote = "Sizin məbləğ üçün az sayda universitet var."
+
+
+if __name__ == '__main__':
+    app.run()
