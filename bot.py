f02c0a3e35ef22b6d775b8a18038bcb77a654bcc
erkin98 <erkinqara@gmail.com>
2021-06-03 12:45:04 +0400

    bot.py port changed

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
+import os
 
 app = Flask(__name__)
 
@@ -37,4 +38,5 @@ def bot():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0',port=port)
