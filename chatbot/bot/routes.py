7135d922ae9065b1dbee04aed1d052970b1ef195
erkin98 <erkinqara@gmail.com>
2021-06-18 16:42:38 +0400

    routes and responses updated

--- chatbot/bot/routes.py
+++ chatbot/bot/routes.py
@@ -20,31 +20,135 @@ def bot():
     msg = resp.message()
     responded = False
     
-    if 'salam' in incoming_msg:
-        quote = 'Salam.Bizə yazdığınız üçün təşəkkürlər.Sizə necə köməklik edə bilərik?'
+    countries = {"ABŞ":"amerika","Almaniya":"almaniya","Avstraliya":"avstraliya","ABS":"amerika",
+        	"Avstriya":"avstriya","Belçika":"belcika","Belcika":"belcika",
+            	"BƏƏ":"bee","Böyük Britaniya":"boyuk-britaniya","İngiltərə":"boyuk-britaniya",
+                "Çexiya":"cexiya","Çin":"cin","Finlandiya":"finlandiya","chexiya":"cexiya","chin":"cin","cin":"cin","cexiya":"cexiya",
+                "Fransa":"fransa","Hollandiya":"hollandiya",'niderland':"hollandiya","İrlandiya":"ireland",
+                "İspaniya":"spain",	"İsveç":"isvec","İsveçrə":"sweden",	"isvec":"Isvec","Isvecre":"sweden",
+                "İtaliya":"italy","Kanada":"kanada","Sinqapur":"sinqapur","Türkiyə":"turkiye","Yeni Zelandiya":"yeni-zelandiya","Turkiye":"turkiye"}
+
+    uni_fee = ['tehsil haqqi','təhsil haqqı','tehsil xerci','təhsil xərci','odenis','ödəniş','odenish','qiymet','qiymət']
+    docs = ['sened' , 'sened qebulu' , 'sənəd' , 'sənəd qəbulu']
+    lang = ['dil bilikleri' , 'dil bilikləri' , 'IELTS' , 'TOEFL']
+    info = ['etrafli melumat' , 'ətraflı məlumat' , 'detalli melumat' , 'detallı məlumat' , 'detal']
+    adres = ['adress' , 'adres' , 'unvan' , 'ünvan' , 'harda' , 'harada']
+    con = ['elaqe' , 'əlaqə' , 'telfon' , 'telefon' , 'nömrə' , 'nomre']
+    res = ['konsultasiya' , 'görüş' , 'gorus' , 'gorush' , "rezervasiya"]
+
+    countries = dict((k.lower(), v.lower()) for k,v in countries.items())
+
+    if 'salam' in incoming_msg :
+        quote = '\'Salam.Siz Azeri Studentin chatbotu ilə əlaqədəsiniz.Sizə necə kömək edə bilərik?\''
 
         msg.body(quote)
         responded = True
 
-    if ('xaricdə təhsil' or "xaricde tehsil") in incoming_msg:
-        quote = 'Xaricdə təhsil üçün büdcəniz nə qədərdir?'
+    elif 'xaricdə təhsil' in incoming_msg :
+        quote1 = 'Hansı ölkənin təhsil müəssisələri ilə maraqlanırsınız?'
+    
+        msg.body(quote1)
+      
+    elif incoming_msg in countries:
+                msg.body('https://azeristudent.az/countries/' + countries[incoming_msg])
+                responded = True
 
-        msg.body(quote)
-        responded = True
+    else:
+        if 'dil kursu' in incoming_msg:
+            quote1 = 'Aktiv dil kursları ilə bağlı məlumat almaq üçün saytımıza daxil olun'
+            quote2 = '--> https://azeristudent.az/language-program/'
 
-    if incoming_msg.isdigit():
-
-        if int(incoming_msg) >= 15000:
-            quote = "Sizə göndərdiyimiz fayldan xidmətlərimizlə tanış ola bilərsiz"
-            msg.body(quote)
+            msg.body(quote1)
+            msg.body(quote2)
             responded = True
-        elif int(incoming_msg) < 10000:
-            quote = "Sizin məbləğ üçün az sayda universitet var."
-            msg.body(quote)
-            responded = True
-    if not responded:
-        quote = "Zəhmət olmasa sualınızı daha anlaşılan formada verin."
-        msg.body(quote)
+            
+
+        else:
+            if incoming_msg in uni_fee:
+
+                quote1 = 'Ödənişlər standart deyil, ətraflı məlumat üçün sizə uyğun düyməni sıxın.Qeyd edək ki,universitetlər 2 hissəli şəkildə ödənişə icazə verir.'
+
+                msg.body(quote1)
+                responded = True
+
+                if 'ali məktəblər' in incoming_msg:
+                    msg.media('https://i.imgur.com/PfeCzCm.jpeg')
+                
+                elif 'orta məktəblər' in incoming_msg:
+                    msg.media('https://i.imgur.com/j0XE3ro.jpeg')
+
+
+            elif  incoming_msg in docs:
+
+                quote1 = 'Tələb olunan sənədlər:3 tövsiyyə məktubu; Akademik esse; Motivasiya məktubu; CV/Resume; Transkriptlər.'
+
+                msg.body(quote1)
+                responded = True
+
+            elif  incoming_msg in lang:
+
+                quote1 = 'Əsasən IELTS və TOEFL dərəcələri tələb olunur.'
+
+                msg.body(quote1)
+                responded = True
+
+            elif  incoming_msg in info:
+
+                quote1 = '''Daha ətraflı məlumatları konsultanlarımızdan əldə edə bilərsiniz.Əgər sizə uyğundursa nömrənizi, ad və soyadınızı qeyd edin, müvafiq əməkdaşımız zəng edib konsultasiya üçün vaxt təyin etsin.'''
+
+                msg.body(quote1)
+                responded = True
+
+
+            elif  incoming_msg in adres:
+
+                quote1 = '''Ofisimiz Bakı şəhəri, 8 Noyabr(Nobel) pr.15, Azure Biznes Mərkəzi 20-ci mərtəbə, ofis - 135 ünvanında yerləşir.'''
+
+                msg.body(quote1)
+                responded = True
+
+            elif  incoming_msg in con:
+
+                quote1 = '''+994505122828
++994502959776
++994124886678'''
+
+                msg.body(quote1)
+                responded = True
+
+            elif  incoming_msg in res:
+
+                quote1 = '''Konsultasiya üçün aşağıda qeyd olunan linkə keçid edin 
+                            --> https://azeristudent.az/reserve/ və ya +994505122828,
+                            +994502959776, 0124886678 nömrələrinə zəng edin.'''
+
+                msg.body(quote1)
+                responded = True
+
+            else:
+                quote1 = 'Davam edə bilmək üçün, zəhmət olmasa, yuxarıdakı düymələrdən birini seçin.'
+
+                msg.body(quote1)
+                responded = True
+    # if ('xaricdə təhsil' or "xaricde tehsil") in incoming_msg:
+    #     quote = 'Xaricdə təhsil üçün büdcəniz nə qədərdir?'
+
+    #     msg.body(quote)
+    #     responded = True
+
+    #     if incoming_msg.isdigit():
+
+    #         if int(incoming_msg) >= 15000:
+    #             quote = "Sizə göndərdiyimiz fayldan xidmətlərimizlə tanış ola bilərsiz"
+    #             msg.body(quote)
+    #             responded = True
+    #         elif int(incoming_msg) < 10000:
+    #             quote = "Sizin məbləğ üçün az sayda universitet var."
+    #             msg.body(quote)
+    #             responded = True
+    #     if not responded:
+    #         quote = "Zəhmət olmasa sualınızı daha anlaşılan formada verin."
+    #         msg.body(quote)
 
     # our_msg = Response(response = quote)
     # db.session.add(our_msg)
--- test.py
+++ test.py
@@ -1,27 +1,27 @@
-from twilio.rest import Client
-import pandas as pd
-from datetime import datetime
+countries = {"ABŞ":"amerika","Almaniya":"almaniya","Avstraliya":"avstraliya",
+        	"Avstriya":"avstriya","Belçika":"belcika",
+            	"BƏƏ":"bee","Böyük Britaniya":"boyuk-britaniya",
+                "Çexiya":"cexiya","Çin":"cin","Finlandiya":"finlandiya",
+                "Fransa":"fransa","Hollandiya":"hollandiya","İrlandiya":"ireland",
+                "İspaniya":"spain",	"İsveç":"isvec","İsveçrə":"sweden",	
+                "İtaliya":"italy","Kanada":"kanada","Sinqapur":"sinqapur","Türkiyə":"turkiye","Yeni Zelandiya":"yeni-zelandiya"}
 
-account_sid = 'AC13ca9197f22f37ed1d02797d60da75d7'
-auth_token = 'ea3708375a819e21b1b02ad4d571a219'
-client = Client(account_sid, auth_token)
+countries = dict((k.lower(), v.lower()) for k,v in countries.items())
 
+incoming_msg = 'Salam.konusltasiya üçün vaxt təyin etmək istərdim'.lower()
 
-messages = client.messages.list(to = 'whatsapp:+16156565203',limit = 10)
-senders = []
-for record in messages:
-    message = client.messages(record.sid).fetch()
-    senders.append(message.from_)
-print(pd.unique(senders))
+# if 'f' in incoming_msg:
+#     print('x')
+#     for i in countries:
+#         print(i)
+#         if i in incoming_msg:
+#             # print(i)
+#             print('https://azeristudent.az/countries/' + countries[incoming_msg])
+        
+if 'konsultasiya' or 'görüş' or 'gorus' or 'gorush' or "rezervasiya" in incoming_msg:
 
+    quote1 = '''Konsultasiya üçün aşağıda qeyd olunan linkə keçid edin 
+                --> https://azeristudent.az/reserve/ və ya +994505122828,
+                +994502959776, 0124886678 nömrələrinə zəng edin.'''
 
-for i in pd.unique(senders):
-    msgs = []
-    messages = client.messages.list(from_ = i,to = 'whatsapp:+16156565203',limit=10)
-    responses = client.messages.list(from_ = 'whatsapp:+16156565203',to = i,limit=10)
-
-    for msg,reply in zip(messages,responses):
-        message = client.messages(msg.sid).fetch()
-        response = client.messages(reply.sid).fetch()
-        msgs.append((message.body,response.body))
-    print((msgs))
+    print(quote1)
