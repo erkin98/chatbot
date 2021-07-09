4df1fd1ec050fe836ae51f99196503631c27a886
erkin98 <erkinqara@gmail.com>
2021-07-09 23:28:15 +0400

    revert

--- test.py
+++ test.py
@@ -1,27 +1,13 @@
-countries = {"ABŞ":"amerika","Almaniya":"almaniya","Avstraliya":"avstraliya",
-        	"Avstriya":"avstriya","Belçika":"belcika",
-            	"BƏƏ":"bee","Böyük Britaniya":"boyuk-britaniya",
-                "Çexiya":"cexiya","Çin":"cin","Finlandiya":"finlandiya",
-                "Fransa":"fransa","Hollandiya":"hollandiya","İrlandiya":"ireland",
-                "İspaniya":"spain",	"İsveç":"isvec","İsveçrə":"sweden",	
-                "İtaliya":"italy","Kanada":"kanada","Sinqapur":"sinqapur","Türkiyə":"turkiye","Yeni Zelandiya":"yeni-zelandiya"}
+from twilio.rest import Client
 
-countries = dict((k.lower(), v.lower()) for k,v in countries.items())
+account_sid = 'AC13ca9197f22f37ed1d02797d60da75d7'
+auth_token = 'ea3708375a819e21b1b02ad4d571a219'
+client = Client(account_sid, auth_token)
 
-incoming_msg = 'Salam.konusltasiya üçün vaxt təyin etmək istərdim'.lower()
+messages = client.messages.list(to = 'whatsapp:+16156565203')
 
-# if 'f' in incoming_msg:
-#     print('x')
-#     for i in countries:
-#         print(i)
-#         if i in incoming_msg:
-#             # print(i)
-#             print('https://azeristudent.az/countries/' + countries[incoming_msg])
-        
-if 'konsultasiya' or 'görüş' or 'gorus' or 'gorush' or "rezervasiya" in incoming_msg:
-
-    quote1 = '''Konsultasiya üçün aşağıda qeyd olunan linkə keçid edin 
-                --> https://azeristudent.az/reserve/ və ya +994505122828,
-                +994502959776, 0124886678 nömrələrinə zəng edin.'''
-
-    print(quote1)
+x = []
+for record in messages:
+    x.append(record.from_)
+    
+print(set(x))
